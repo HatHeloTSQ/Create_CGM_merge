c9e82f78d79ffcd4413cdfd682643e1e79b6411e
Karikó-Tóth Máté <karikototh.mate02@gmail.com>
2023-08-04 00:07:29 +0200

    Added script to add transitional items
    This script is responsible for the custom items you get during sequenced assembly.
    - Added all bullet types
    - Added all grenades
    - Added all scopes
    - Added silencer

---
+++ kubejs/startup_scripts/c-cgm_items.js
@@ -0,0 +1,50 @@
+StartupEvents.registry('item', event => {
+    event.create('unfinished_basic_bullet').texture('unfinished_basic_bullet').modelJson('unfinished_basic_bullet.json')
+    }
+)
+
+StartupEvents.registry('item', event => {
+    event.create('unfinished_advanced_bullet').texture('unfinished_advanced_bullet').modelJson('unfinished_advanced_bullet.json')
+    }
+)
+
+StartupEvents.registry('item', event => {
+    event.create('unfinished_shell').texture('unfinished_shell').modelJson('unfinished_shell.json')
+    }
+)
+
+StartupEvents.registry('item', event => {
+    event.create('unfinished_missile').texture('unfinished_missile').modelJson('unfinished_missile.json')
+    }
+)
+
+StartupEvents.registry('item', event => {
+    event.create('unfinished_frag_grenade').texture('unfinished_frag_grenade').modelJson('unfinished_frag_grenade.json')
+    }
+)
+
+StartupEvents.registry('item', event => {
+    event.create('unfinished_stun_grenade').texture('unfinished_stun_grenade').modelJson('unfinished_stun_grenade.json')
+    }
+)
+
+StartupEvents.registry('item', event => {
+    event.create('unfinished_short_scope').texture('unfinished_short_scope').modelJson('unfinished_short_scope.json')
+    }
+)
+
+StartupEvents.registry('item', event => {
+    event.create('unfinished_medium_scope').texture('unfinished_medium_scope').modelJson('unfinished_medium_scope.json')
+    }
+)
+
+StartupEvents.registry('item', event => {
+    event.create('unfinished_long_scope').texture('unfinished_long_scope').modelJson('unfinished_long_scope.json')
+    }
+)
+
+StartupEvents.registry('item', event => {
+    event.create('unfinished_basic_silencer').texture('unfinished_basic_silencer').modelJson('unfinished_basic_silencer.json')
+    }
+)
+
