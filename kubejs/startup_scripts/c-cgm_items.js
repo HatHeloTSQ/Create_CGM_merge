3fb82efc7be097990ed1be7ee712687e774825f1
Karikó-Tóth Máté <karikototh.mate02@gmail.com>
2023-08-21 21:18:10 +0200

    Fixed issue with custom items
    Added max size for everything:
    - All bullets (64)
    - Frag and Stun Grenades (64)
    - All attachments (1)
    - All weapons (1)

--- kubejs/startup_scripts/c-cgm_items.js
+++ kubejs/startup_scripts/c-cgm_items.js
@@ -1,119 +1,119 @@
 StartupEvents.registry('item', event => {
-    event.create('unfinished_basic_bullet').texture('unfinished_basic_bullet').modelJson('unfinished_basic_bullet.json')
+    event.create('unfinished_basic_bullet').texture('unfinished_basic_bullet').modelJson('unfinished_basic_bullet.json').maxStackSize(64)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_advanced_bullet').texture('unfinished_advanced_bullet').modelJson('unfinished_advanced_bullet.json')
+    event.create('unfinished_advanced_bullet').texture('unfinished_advanced_bullet').modelJson('unfinished_advanced_bullet.json').maxStackSize(64)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_shell').texture('unfinished_shell').modelJson('unfinished_shell.json')
+    event.create('unfinished_shell').texture('unfinished_shell').modelJson('unfinished_shell.json').maxStackSize(64)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_missile').texture('unfinished_missile').modelJson('unfinished_missile.json')
+    event.create('unfinished_missile').texture('unfinished_missile').modelJson('unfinished_missile.json').maxStackSize(64)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_frag_grenade').texture('unfinished_frag_grenade').modelJson('unfinished_frag_grenade.json')
+    event.create('unfinished_frag_grenade').texture('unfinished_frag_grenade').modelJson('unfinished_frag_grenade.json').maxStackSize(64)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_stun_grenade').texture('unfinished_stun_grenade').modelJson('unfinished_stun_grenade.json')
+    event.create('unfinished_stun_grenade').texture('unfinished_stun_grenade').modelJson('unfinished_stun_grenade.json').maxStackSize(64)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_short_scope').texture('unfinished_short_scope').modelJson('unfinished_short_scope.json')
+    event.create('unfinished_short_scope').texture('unfinished_short_scope').modelJson('unfinished_short_scope.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_medium_scope').texture('unfinished_medium_scope').modelJson('unfinished_medium_scope.json')
+    event.create('unfinished_medium_scope').texture('unfinished_medium_scope').modelJson('unfinished_medium_scope.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_long_scope').texture('unfinished_long_scope').modelJson('unfinished_long_scope.json')
+    event.create('unfinished_long_scope').texture('unfinished_long_scope').modelJson('unfinished_long_scope.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_basic_silencer').texture('unfinished_basic_silencer').modelJson('unfinished_basic_silencer.json')
+    event.create('unfinished_basic_silencer').texture('unfinished_basic_silencer').modelJson('unfinished_basic_silencer.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_light_stock').texture('unfinished_light_stock').modelJson('unfinished_light_stock.json')
+    event.create('unfinished_light_stock').texture('unfinished_light_stock').modelJson('unfinished_light_stock.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_weighted_stock').texture('unfinished_weighted_stock').modelJson('unfinished_weighted_stock.json')
+    event.create('unfinished_weighted_stock').texture('unfinished_weighted_stock').modelJson('unfinished_weighted_stock.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_tactical_stock').texture('unfinished_tactical_stock').modelJson('unfinished_weighted_tactical.json')
+    event.create('unfinished_tactical_stock').texture('unfinished_tactical_stock').modelJson('unfinished_weighted_tactical.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_light_grip').texture('unfinished_light_grip').modelJson('unfinished_light_grip.json')
+    event.create('unfinished_light_grip').texture('unfinished_light_grip').modelJson('unfinished_light_grip.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_specialised_grip').texture('unfinished_specialised_grip').modelJson('unfinished_specialised_grip.json')
+    event.create('unfinished_specialised_grip').texture('unfinished_specialised_grip').modelJson('unfinished_specialised_grip.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_standard_pistol').texture('unfinished_standard_pistol').modelJson('unfinished_standard_pistol.json')
+    event.create('unfinished_standard_pistol').texture('unfinished_standard_pistol').modelJson('unfinished_standard_pistol.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_standard_shotgun').texture('unfinished_standard_shotgun').modelJson('unfinished_standard_shotgun.json')
+    event.create('unfinished_standard_shotgun').texture('unfinished_standard_shotgun').modelJson('unfinished_standard_shotgun.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_standard_machine_pistol').texture('unfinished_standard_machine_pistol').modelJson('unfinished_standard_machine_pistol.json')
+    event.create('unfinished_standard_machine_pistol').texture('unfinished_standard_machine_pistol').modelJson('unfinished_standard_machine_pistol.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_standard_rifle').texture('unfinished_standard_rifle').modelJson('unfinished_standard_rifle.json')
+    event.create('unfinished_standard_rifle').texture('unfinished_standard_rifle').modelJson('unfinished_standard_rifle.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_standard_assault_rifle').texture('unfinished_standard_assault_rifle').modelJson('unfinished_standard_assault_rifle.json')
+    event.create('unfinished_standard_assault_rifle').texture('unfinished_standard_assault_rifle').modelJson('unfinished_standard_assault_rifle.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_marksman_rifle').texture('unfinished_marksman_rifle').modelJson('unfinished_marksman_rifle.json')
+    event.create('unfinished_marksman_rifle').texture('unfinished_marksman_rifle').modelJson('unfinished_marksman_rifle.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_bazooka').texture('unfinished_bazooka').modelJson('unfinished_bazooka.json')
+    event.create('unfinished_bazooka').texture('unfinished_bazooka').modelJson('unfinished_bazooka.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_grenade_launcher').texture('unfinished_grenade_launcher').modelJson('unfinished_grenade_launcher.json')
+    event.create('unfinished_grenade_launcher').texture('unfinished_grenade_launcher').modelJson('unfinished_grenade_launcher.json').maxStackSize(1)
     }
 )
 
 StartupEvents.registry('item', event => {
-    event.create('unfinished_minigun').texture('unfinished_minigun').modelJson('unfinished_minigun.json')
+    event.create('unfinished_minigun').texture('unfinished_minigun').modelJson('unfinished_minigun.json').maxStackSize(1)
     }
 )
