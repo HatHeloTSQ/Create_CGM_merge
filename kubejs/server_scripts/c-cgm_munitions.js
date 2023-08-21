31633879380b4ff118874b7a57cfc198faad20a9
Karikó-Tóth Máté <karikototh.mate02@gmail.com>
2023-08-21 21:18:32 +0200

    Created script with munition recipes

---
+++ kubejs/server_scripts/c-cgm_munitions.js
@@ -0,0 +1,9 @@
+// This script is reponsible for the crafting recipes of all base cgm bullets and grenades
+
+ServerEvents.recipes(e => {
+    
+    const bullet1 =     'kubejs:unfinished_basic_bullet'
+
+    // Basic Bullet
+    
+  })
